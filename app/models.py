1e6ff145f7017dccb03bbc9957fe3d586f62e418
amiko <caihaifei@enjoytoday.cn>
2018-01-20 01:01:17 +0800

    更新.

--- app/models.py
+++ app/models.py
@@ -3,21 +3,31 @@
 from . import db
 from datetime import datetime
 
+
 class ShareCategory(db.Model):
     u"""
      分享类别信息
     """
-    id = db.Column(db.Integer,primary_key = True)
+    __tablename__ = "share_category"
+    id = db.Column(db.Integer,primary_key = True,autoincrement=True)
     name = db.Column(db.String(50),unique =True)
     summary = db.Column(db.String(300))
     icon = db.Column(db.String(100),unique =True)
     member_since = db.Column(db.DateTime(),default = datetime.utcnow())
 
 
-
-class ShareTopic(db.Model):
+class Topic(db.Model):
     u"""
-    分享话题
+    分享话题,每次分享都是一个话题,话题从属于一个Category.
     """
-    id = db.Column(db.Integer,primary_key = True)
-    title = db.Column(db.String(100),unique =True)
+    __tablename__ = "topics"
+    id = db.Column(db.Integer,primary_key = True,autoincrement=True)
+    name = db.Column(db.String(100), unique=True)
+    title = db.Column(db.String(100))
+    member_since = db.Column(db.DateTime(),default = datetime.utcnow())
+
+    # 添加外键
+    category_id = db.Column(db.Integer,db.ForeignKey('share_category.id'))
+
+
+
--- app/share/views.py
+++ app/share/views.py
@@ -3,7 +3,7 @@
 from flask import render_template,session,redirect,url_for
 # 导入蓝本 main
 from . import share
-from ..models import ShareCategory
+from ..models import ShareCategory,Topic
 
 # 导入配置参数
 from app.main.config import *
@@ -12,12 +12,33 @@ context = pageDebugContext
 
 @share.route('/')
 @share.route('/index.html')
-@share.route('/index.html')
+@share.route('/index')
 def index():
     return render_template('share/index.html',context = context)
 
 
+@share.route('/category')
+def categorys():
+    u"""
+    :return:
+    """
+    return render_template('share/show.html', context=context)
+
+
 @share.route('/category/<name>')
-def category(name):
-    category = ShareCategory(name =name)
+def child_category(name):
+    category = ShareCategory.query.filter(name =name).first()
+    if category is None:
+        return render_template('share/category.html',context=context)
     return render_template('share/category.html',context =context,category = category)
+
+
+@share.route('/category/<category_name>/<topic_name>')
+def topic(category_name,topic_name):
+    category = ShareCategory.query.filter(name = category_name).first()
+    if category is not None :
+        topic = Topic.query.filter(category_id = category.id ,name = topic_name)
+        if topic is not None:
+            return render_template('share/topic.html', context=context,category = category,topic = topic)
+    return render_template('share/no_found.html',context=context)
+
